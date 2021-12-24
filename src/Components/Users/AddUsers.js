5e8ba4aa73e81e65ae9f4b41fa610e514fe2fe1d
FAISAL QURESHI <qureshifi@icloud.com>
2021-12-24 20:55:01 +0530

    first commit

---
+++ src/Components/Users/AddUsers.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+
+import classes from "./AddUsers.module.css";
+
+const AddUser = (props) => {
+  const [enteredUserName, setEnteredUserName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  const userNameChangeHandler = (event) => {
+    setEnteredUserName(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" onChange={userNameChangeHandler} />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" onChange={ageChangeHandler} />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
